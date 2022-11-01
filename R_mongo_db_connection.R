240aad147a6fd6a02775602ca5bbc706a1e873ed
Reddy7860 <114321466+Reddy7860@users.noreply.github.com>
2022-11-01 14:38:13 -0400

    Update R_mongo_db_connection.R

--- R_mongo_db_connection.R
+++ R_mongo_db_connection.R
@@ -4,10 +4,11 @@ connection_string = "mongodb+srv://Titania:Mahadev@cluster0.zq3w2cn.mongodb.net/
 
 final_orders_raw_data_collection = mongo(collection="final_orders_raw_data", db="titania_trading", url=connection_string)
 
-temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }}',"")
+# temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }}',"")
+temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }',',"Stock":"%5ENSEI" }')
 
 query_output = final_orders_raw_data_collection$find(query = temp_query,sort = '{"Datetime" : 1}')
 
 algo_orders_place_data_collection = mongo(collection="algo_orders_place_data", db="titania_trading", url=connection_string)
-temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }',',"client_id":"J95213" }')
+temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }',',"client_id":"J95213","Stock":"%5ENSEI" }')
 query_output = algo_orders_place_data_collection$find(query = temp_query,sort = '{"Datetime" : 1}')
