f6a436d7c5d062fefdabbdb417902bce34b98aec
Reddy7860 <114321466+Reddy7860@users.noreply.github.com>
2022-11-01 13:00:43 -0400

    Update R_mongo_db_connection.R

--- R_mongo_db_connection.R
+++ R_mongo_db_connection.R
@@ -4,5 +4,7 @@ connection_string = "mongodb+srv://Titania:Mahadev@cluster0.zq3w2cn.mongodb.net/
 
 final_orders_raw_data_collection = mongo(collection="final_orders_raw_data", db="titania_trading", url=connection_string)
 
-query_output = final_orders_raw_data_collection$find('{"execution_date":Sys.Date()}',sort = '{"Datetime" : -1}')
+temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }}',"")
+
+query_output = final_orders_raw_data_collection$find(query = temp_query,sort = '{"Datetime" : -1}')
 
