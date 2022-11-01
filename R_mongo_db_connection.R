83cfc69aa0fc77d17c01fe6bf17026f1ccf48122
Reddy7860 <114321466+Reddy7860@users.noreply.github.com>
2022-11-01 14:27:13 -0400

    Update R_mongo_db_connection.R

--- R_mongo_db_connection.R
+++ R_mongo_db_connection.R
@@ -8,3 +8,6 @@ temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()),
 
 query_output = final_orders_raw_data_collection$find(query = temp_query,sort = '{"Datetime" : 1}')
 
+algo_orders_place_data_collection = mongo(collection="algo_orders_place_data_collection", db="titania_trading", url=connection_string)
+temp_query = paste0('{"execution_date": { "$gte" :  "',as.character(Sys.Date()), '" }',',"client_id":"J95213" }')
+query_output = algo_orders_place_data_collection$find(query = temp_query,sort = '{"Datetime" : 1}')
