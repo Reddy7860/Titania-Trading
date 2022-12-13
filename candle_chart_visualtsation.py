1650c73e87aedaab6193fbd7dc5975971e400f8b
Reddy7860 <114321466+Reddy7860@users.noreply.github.com>
2022-12-12 23:25:13 -0500

    Update candle_chart_visualtsation.py

--- candle_chart_visualtsation.py
+++ candle_chart_visualtsation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from datetime import timedelta
 
 
 server_api = ServerApi('1')
@@ -10,11 +11,11 @@ server_api = ServerApi('1')
 client = MongoClient("mongodb+srv://Titania:Mahadev@cluster0.zq3w2cn.mongodb.net/titania_trading?ssl=true&ssl_cert_reqs=CERT_NONE", server_api=server_api)
 db = client["titania_trading"]
 
-Stocks_data_5_minutes = db["Stocks_data_5_minutes"].find({'Stock':"Nifty",'instrumenttype':"FUTIDX"}).sort([('Datetime', 1)])
+Stocks_data_5_minutes = db["Stocks_data_5_minutes"].find({'Stock':"BankNifty",'instrumenttype':"FUTIDX"}).sort([('Datetime', 1)])
 Stocks_data_5_minutes =  pd.DataFrame(list(Stocks_data_5_minutes))
 
 
-support_and_resistance = db["support_and_resistance"].find({'Stock':"Nifty"}).sort([('Execution_date', 1)])
+support_and_resistance = db["support_and_resistance"].find({'Stock':"BankNifty"}).sort([('Execution_date', 1)])
 support_and_resistance =  pd.DataFrame(list(support_and_resistance))
 
 
@@ -27,12 +28,13 @@ print(support_and_resistance.columns)
 
 modified_stocks_5_data = Stocks_data_5_minutes.merge(support_and_resistance, left_on='Execution_Date', right_on='Execution_date',how="left")
 
+
+modified_stocks_5_data['Datetime'] = modified_stocks_5_data['Datetime'] + timedelta(hours=5,minutes=30)
+
 fig = go.Figure(data=[go.Candlestick(x=modified_stocks_5_data['Datetime'],
                 open=modified_stocks_5_data['Open'], high=modified_stocks_5_data['High'],
                 low=modified_stocks_5_data['Low'], close=modified_stocks_5_data['Close'])
                      ])
-
-fig.layout = dict(xaxis=dict(type="category"))
 
 fig.add_trace(
     go.Scatter(mode = "lines",
@@ -52,8 +54,30 @@ fig.add_trace(
         y=modified_stocks_5_data['arima_support_1']
     ))
 
+fig.layout = dict(xaxis=dict(type="category"))
+
 fig.update_layout(xaxis_rangeslider_visible=False)
 fig.update_xaxes(rangebreaks=[dict(bounds=["sat", "mon"])]) 
 
+fig.update_layout(
+    autosize=False,
+    width=1000,
+    height=800,)
+
 
 fig.show()
+
+
+algo_orders_place_data = db["algo_orders_place_data"].find({'client_id':"J95213","Stock":"%5ENSEBANK"})
+algo_orders_place_data =  pd.DataFrame(list(algo_orders_place_data))
+
+modified_stocks_5_data['Execution_Date'] = pd.to_datetime(modified_stocks_5_data['Execution_Date'])
+
+algo_orders_place_data = algo_orders_place_data[['Strategy', 'Stock', 'Signal','Datetime', 'Value','buy_probability', 'sell_probability','current_script','Strike_Buy_Price','historic_profit','conclusion']]
+
+modified_stocks_5_data = modified_stocks_5_data.merge(algo_orders_place_data, left_on='Datetime', right_on='Datetime',how="left")
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+modified_stocks_5_data
