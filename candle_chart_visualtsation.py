917884483f7c76750f63cbdc12c53887b21c4011
Reddy7860 <114321466+Reddy7860@users.noreply.github.com>
2022-12-13 10:50:00 -0500

    Update candle_chart_visualtsation.py

--- candle_chart_visualtsation.py
+++ candle_chart_visualtsation.py
@@ -31,6 +31,25 @@ modified_stocks_5_data = Stocks_data_5_minutes.merge(support_and_resistance, lef
 
 modified_stocks_5_data['Datetime'] = modified_stocks_5_data['Datetime'] + timedelta(hours=5,minutes=30)
 
+
+
+algo_orders_place_data = db["algo_orders_place_data"].find({'client_id':"J95213","Stock":"%5ENSEBANK"})
+algo_orders_place_data =  pd.DataFrame(list(algo_orders_place_data))
+
+modified_stocks_5_data['Execution_Date'] = pd.to_datetime(modified_stocks_5_data['Execution_Date'])
+
+algo_orders_place_data = algo_orders_place_data[['Strategy', 'Stock', 'Signal','Datetime', 'Value','buy_probability', 'sell_probability','current_script','Strike_Buy_Price','premium_Target','premium_StopLoss','historic_profit','conclusion']]
+
+modified_stocks_5_data = modified_stocks_5_data.merge(algo_orders_place_data, left_on='Datetime', right_on='Datetime',how="left")
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+modified_stocks_5_data
+
+
+
 fig = go.Figure(data=[go.Candlestick(x=modified_stocks_5_data['Datetime'],
                 open=modified_stocks_5_data['Open'], high=modified_stocks_5_data['High'],
                 low=modified_stocks_5_data['Low'], close=modified_stocks_5_data['Close'])
@@ -54,6 +73,28 @@ fig.add_trace(
         y=modified_stocks_5_data['arima_support_1']
     ))
 
+# fig.add_trace(
+#     go.Scatter(
+#         x=modified_stocks_5_data['Datetime'],
+#         y=modified_stocks_5_data["Strategy_x"],
+#         mode="markers+text",
+#         marker=dict(symbol='triangle-down-open', size = 12),
+# #         text = 'important',
+# #         textposition = 'middle right'
+
+#     )
+# )
+
+fig.add_trace(
+    go.Scatter(
+        x=["2022-12-13 09:20:00"],
+        y=["43048.47"],
+        mode="markers",
+        marker=dict(symbol='triangle-down-open')
+
+    )
+)
+
 fig.layout = dict(xaxis=dict(type="category"))
 
 fig.update_layout(xaxis_rangeslider_visible=False)
@@ -66,18 +107,3 @@ fig.update_layout(
 
 
 fig.show()
-
-
-algo_orders_place_data = db["algo_orders_place_data"].find({'client_id':"J95213","Stock":"%5ENSEBANK"})
-algo_orders_place_data =  pd.DataFrame(list(algo_orders_place_data))
-
-modified_stocks_5_data['Execution_Date'] = pd.to_datetime(modified_stocks_5_data['Execution_Date'])
-
-algo_orders_place_data = algo_orders_place_data[['Strategy', 'Stock', 'Signal','Datetime', 'Value','buy_probability', 'sell_probability','current_script','Strike_Buy_Price','historic_profit','conclusion']]
-
-modified_stocks_5_data = modified_stocks_5_data.merge(algo_orders_place_data, left_on='Datetime', right_on='Datetime',how="left")
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-
-modified_stocks_5_data
